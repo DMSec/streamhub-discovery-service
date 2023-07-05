9f0e89366f3ef0e7ee4768101a97da4d2156b082
douglasmsi <douglas.marra@mercadolivre.com>
2023-07-04 21:19:10 -0300

    Initial discovery service

---
+++ src/test/java/br/com/dmsec/discoveryservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.dmsec.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	// Teste de contexto
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
